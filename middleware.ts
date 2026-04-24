bc1db36477505b35b7cdbfa63b3d738e2ac62887
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-04-24 08:20:41 +0530

    maintain over

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+  {/*import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
@@ -13,5 +13,5 @@ export function middleware(request: NextRequest) {
 
 // Apply to all routes
 export const config = {
-  matcher: "/((?!maintenance).*)",
-};
+  matcher: "/((?!maintenance).*)", 
+};  */}
