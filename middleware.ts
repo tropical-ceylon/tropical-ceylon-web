19bddd1e07055d2f0b3732dfe08cce6ce5f561e4
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-05-08 16:08:30 +0530

    maintain mode off

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const hostname = request.headers.get("host");
 
-  const isMaintenance = true; // 👈 turn ON/OFF here
+  const isMaintenance = false; // 👈 turn ON/OFF here
 
   if (isMaintenance && hostname?.includes("tropicalceylontravels.com")) {
     return NextResponse.redirect(new URL("/maintenance", request.url));
